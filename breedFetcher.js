4e3c91c45def83a4973c27d01c36e8c2ddd9b0cf
Safia Abdale <safia.abdale@gmail.com>
2021-12-06 13:40:30 +0000

    Moved all logging to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,16 @@
 const request = require('request'); // For HTTP request
 const args = process.argv.slice(2); // Takes cmd line arguments
-let inputBreedName = args[0];
+let breedName = args[0];
 
-const fetchBreedDescription = function(inputBreedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
 
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${inputBreedName}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     // Desterilize the string to an object so we can sort through it:
     const data = JSON.parse(body);
-  
-    if (error) {
-      // Edge case: if request function fails
-      console.log (`🚨 ${error}`); // prints details
-    } else if (data[0] === undefined) {
-      // Edge case: if breed is not found
-      console.log (`🚨 Error: Breed not found`);
-    } else {
-      // Output description:
-      console.log (`🐈 DESCRIPTION 🧶 ${'\n'}-${'\n'}🐾 ${data[0].description}`);
-    }
+
+    let desc = `🐈 DESCRIPTION 🧶 ${'\n'}-${'\n'}🐾 ${data[0].description}`;
+
+    callback(error, desc);
   });
 };
 
